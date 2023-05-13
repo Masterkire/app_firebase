055063968db2c0715de0e72b00c7e78e888ad66c
Master Erik <davidmasterkire@gmail.com>
2023-05-12 18:01:13 -0600

    Solucionamos el problema de las variables de entorno ya que no accedia con process por iniciar este proyecto con vite, pero marca error al conectarse a base de datos si se dejan las variables

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -1,6 +1,7 @@
 // Importamos la funcion que necesita el SDK de firebase para iniciar
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+// import.meta.env;
 
 // TODO: Añade el SDKs que necesites utilizar
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,12 +17,12 @@ const firebaseConfig = {
 };
 
 /* const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID
 }; */
 
 // Iicializando firebase
